5a6c021bc54428240c9061d0d79b0c99d23d9bf1
kemboi <evanskemb@gmail.com>
2023-02-27 01:18:58 +0300

    Added permuation

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,30 @@ public class Main {
         String str = "hellouniquestring?#";
         System.out.println(uniqueString(str));
 
+        System.out.println("Permutation");
+        String str1 = "toc";
+        String str2 = "cot ";
+        System.out.println(permutation(str1,str2));
+
+    }
+
+    private static boolean permutation(String str1, String str2) {
+        /*Given two strings, determine whether one is a permutation of the other*/
+        if (str1.length() != str2.length()) return  false;
+        int [] char_count = new int[128];
+        char[] str1_Array = str1.toCharArray();
+        for (char c : str1_Array){
+            char_count[c]++;
+        }
+        for (int i = 0; i < str2.length(); i++){
+            int c = (int) str2.charAt(i);
+            char_count[c]--;
+            if (char_count[c] < 0){
+                    return false;
+            }
+        }
+
+        return true;
     }
 
     private static boolean uniqueString(String str) {

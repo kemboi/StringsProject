37a99a5be31b4c7f2471cfb16bf1713dda047fd7
kemboi <evanskemb@gmail.com>
2023-02-26 21:15:00 +0300

    added a unqiueString()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Determining whether " +
+                "a string has unique characters without using " +
+                "extra data structures");
+        String str = "hellouniquestring?#";
+        System.out.println(uniqueString(str));
+
+    }
+
+    private static boolean uniqueString(String str) {
+        if (str.length() > 128) return false; // a string with more than 128 characters cannot be unique
+        boolean [] charSet = new boolean[128]; // ASCII character set
+
+        for (int i = 0; i < str.length(); i++){
+            int val = str.charAt(i);
+            if(charSet[val]){
+                return false;
+            }
+            charSet[val] = true;
+        }
+        return true;
     }
 }

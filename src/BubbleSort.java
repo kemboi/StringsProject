5b528bb9e73e545383e5e639adf8bb985c707891
kemboi <evanskemb@gmail.com>
2023-03-31 22:13:53 +0300

    Added defination for selection sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -37,9 +37,18 @@ public class BubbleSort {
 
 }
 class SelectionSort{
-    /*Keep finding the maximum or minimum element in the array and place the element
-    * in its correct index
-    * The running time of this algorithm is same as that of Bubble Sort*/
+    /*Keep finding the maximum or minimum element in the array and place the element in its correct index
+    Selection sort is a simple and efficient sorting algorithm that works by repeatedly selecting
+     the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted
+     portion of the list. The algorithm repeatedly selects the smallest (or largest) element from the
+     unsorted portion of the list and swaps it with the first element of the unsorted portion.
+     This process is repeated for the remaining unsorted portion of the list until the entire list is sorted.
+     One variation of selection sort is called “Bidirectional selection sort” which goes through the list of elements
+      by alternating between the smallest and largest element, this way the algorithm can be faster in some cases.
+    *  Defination adopted from GeeksforGeeks (2023) : https://www.geeksforgeeks.org/selection-sort/
+    * The running time of this algorithm is same as that of Bubble Sort
+    *
+    * */
     public int[] selectionsort(int[] arr){
             for (int i = 0; i < arr.length; i++){
                 // find the maximum item in the remaining array and swap with the correct index.
